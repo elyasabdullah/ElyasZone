83542e0aaed4fefe967bc2b459d3e3f1b40d5df6
Elyas Fahmi Ahmed Abdullah <elyasabdullah771124@gmail.com>
2024-04-26 05:26:33 +0300

    Addin the elyaszone.vercel.app to the allowedOrigins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://elyaszone.onrender.com'
+    'https://elyaszone.onrender.com',
+    'https://elyaszone.vercel.app'
 ];
 
 module.exports = allowedOrigins;
